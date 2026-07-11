4bd080c210e1d1bad678e880d75d3679d6c22731
bobbyginn09-sys <bobbyginn09@yahoo.com>
2026-07-11 13:52:31 -0500

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'pf-workout-tracker-v7';
+const CACHE_NAME = 'pf-workout-tracker-v9';
 const ASSETS = ['./', './index.html', './manifest.webmanifest', './icons/icon-192.png', './icons/icon-512.png'];
 
 self.addEventListener('install', event => {
